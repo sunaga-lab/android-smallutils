543bf62e50aacae212ee9d7439742c183e1e5a64
SUNAGA Takahiro <sunaga@sunaga-lab.com>
2021-12-12 13:20:35 +0900

    Add nullable composer

--- smallutils/src/main/java/com/sunagalab/smallutils/LiveDataUtils.kt
+++ smallutils/src/main/java/com/sunagalab/smallutils/LiveDataUtils.kt
@@ -3,6 +3,7 @@ package com.sunagalab.smallutils
 import androidx.lifecycle.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.withTimeoutOrNull
+import java.lang.AssertionError
 
 
 object LiveDataTools {
@@ -79,6 +80,63 @@ object LiveDataTools {
             p4Val = it
             update()
         }
+        return result
+    }
+
+    class Unsettable<T>() {
+
+        val isSet: Boolean
+            get() = !valueWrapped.isEmpty()
+        private var valueWrapped = listOf<T>()
+
+        fun unsetValue() {
+            valueWrapped = listOf()
+        }
+
+        var value: T
+            get() {
+                assert(!valueWrapped.isEmpty())
+                return valueWrapped[0]
+            }
+            set(newValue) {
+                valueWrapped = listOf(newValue)
+            }
+
+    }
+
+    fun <T1, T2, TResult> composeNullable(p1: LiveData<T1>, p2: LiveData<T2>, composer: (T1, T2)->TResult): LiveData<TResult> {
+        val result = MediatorLiveData<TResult>()
+        val p1Val = Unsettable<T1>()
+        val p2Val = Unsettable<T2>()
+        fun<T> addSrc(liveData: LiveData<T>, stockValue: Unsettable<T>) {
+            result.addSource(liveData) {
+                stockValue.value = it
+                if(p1Val.isSet && p2Val.isSet) {
+                    result.postValue(composer(p1Val.value, p2Val.value))
+                }
+            }
+        }
+        addSrc(p1, p1Val)
+        addSrc(p2, p2Val)
+        return result
+    }
+
+    fun <T1, T2, T3, TResult> composeNullable(p1: LiveData<T1>, p2: LiveData<T2>, p3: LiveData<T3>, composer: (T1, T2, T3)->TResult): LiveData<TResult> {
+        val result = MediatorLiveData<TResult>()
+        val p1Val = Unsettable<T1>()
+        val p2Val = Unsettable<T2>()
+        val p3Val = Unsettable<T3>()
+        fun<T> addSrc(liveData: LiveData<T>, stockValue: Unsettable<T>) {
+            result.addSource(liveData) {
+                stockValue.value = it
+                if(p1Val.isSet && p2Val.isSet && p3Val.isSet) {
+                    result.postValue(composer(p1Val.value, p2Val.value, p3Val.value))
+                }
+            }
+        }
+        addSrc(p1, p1Val)
+        addSrc(p2, p2Val)
+        addSrc(p3, p3Val)
         return result
     }
 
